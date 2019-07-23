e5d70bce3bf747e1b9c11ec378b2d17808538740
Giang Nguyen <hust2412@gmail.com>
2019-07-23 01:54:23 -0400

    lcs memoize

--- dynamic_programming/lcs.py
+++ dynamic_programming/lcs.py
@@ -1,3 +1,5 @@
+import timeit
+
 def lcs(s1, s2):
     s1_len = len(s1)
     s2_len = len(s2)
@@ -21,9 +23,49 @@ def lcs(s1, s2):
     
     return lcs_cnt
 
-s1 = 'ABCDGH'
-s2 = 'AEDFHR'
-print(s1, s2, lcs(s1, s2))
+def lcs_memoize(s1, s2, dct = {}):
+    if ((s1 + '0' + s2) in dct):
+        return dct[(s1 + '0' + s2)]
+    s1_len = len(s1)
+    s2_len = len(s2)
+
+    lcs_solution = []
+    lcs_cnt = 0
+
+    if (s1_len == 0 or s2_len == 0):
+        return 0
+
+    while (s1_len > 0 and s2_len > 0):
+        if (s1[s1_len - 1] == s2[s2_len - 1]):
+            lcs_solution.append(s1[s1_len - 1])
+            lcs_cnt += 1
+
+            s1_len -= 1
+            s2_len -= 1
+        else:
+            lcs_cnt += max(lcs_memoize(s1[:s1_len - 1], s2[:s2_len], dct), lcs_memoize(s1[:s1_len], s2[:s2_len - 1], dct))
+            break
+    
+    dct[(s1 + '0' + s2)] = lcs_cnt #Memoize here
+
+    return lcs_cnt
+
+s1 = 'ASDGHJKLBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
+s2 = 'ASDQWEbGBHNJKMLLAABBBBBBBBBBBBBAAAAAAAAAAAAA'
+
+start = timeit.default_timer()
+#s1 = 'ABCDGH'
+#s2 = 'AEDFHR'
+#print(s1, s2, lcs(s1, s2))
+stop = timeit.default_timer()
+print('Eslaped Time naive: ', stop - start)
+
+start = timeit.default_timer()
+#s1 = 'ABCDGH'
+#s2 = 'AEDFHR'
+print(s1, s2, lcs_memoize(s1, s2))
+stop = timeit.default_timer()
+print('Eslaped Time memoize: ', stop - start)
 
 s1 = 'AAAAA'
 s2 = 'AAABA'
