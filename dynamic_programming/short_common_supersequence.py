07b5d91c6b6b8f3fbc0fb7b3c7c6146384d0d4b5
Giang Nguyen <hust2412@gmail.com>
2019-08-07 08:30:01 -0400

    SCS problem

--- dynamic_programming/short_common_supersequence.py
+++ dynamic_programming/short_common_supersequence.py
@@ -1,25 +1,30 @@
 # Using dp to solve this problem
-# Given 2 strings str1 and str2, find the len of shortest string that has both str1 and str2 as subsequences
-def shortestCommonSuperSequenceLen(str1, str2):
+# Given 2 strings str1 and str2, find the shortest string that has both str1 and str2 as subsequences
+def shortestCommonSuperSequence(str1, str2):
     str1_len = len(str1)
     str2_len = len(str2)
 
     if (str1_len == 0):
-        return str2_len
+        return str2
     elif(str2_len == 0):
-        return str1_len
+        return str1
 
     if (str1[-1] == str2[-1]):
-        return 1 + shortestCommonSuperSequenceLen(str1[:-1], str2[:-1])
+        return shortestCommonSuperSequence(str1[:-1], str2[:-1]) + str1[-1]
     else:
-        return 1 + min(shortestCommonSuperSequenceLen(str1[:-1], str2), shortestCommonSuperSequenceLen(str1, str2[:-1]))
+        solution1 = shortestCommonSuperSequence(str1[:-1], str2)
+        solution2 = shortestCommonSuperSequence(str1, str2[:-1])
+        if len(solution1) < len(solution2):
+            return shortestCommonSuperSequence(str1[:-1], str2) + str1[-1]
+        else:
+            return shortestCommonSuperSequence(str1, str2[:-1]) + str2[-1]
 
 str1 = "geek"
 str2 = "eke"
 
-print(shortestCommonSuperSequenceLen(str1, str2))
+print(shortestCommonSuperSequence(str1, str2))
 
 str1 = "AGGTAB"
 str2 = "GXTXAYB"
 
-print(shortestCommonSuperSequenceLen(str1, str2))
+print(shortestCommonSuperSequence(str1, str2))
